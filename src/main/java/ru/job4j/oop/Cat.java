9677b4042acf26a088655f15e9f893231770730d
Biryukov-ala <abbs2305@gmail.com>
2021-11-03 14:49:29 +0700

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,15 +1,57 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+    private String food;
+    private String name;
+
+    public void showFood() {
+        System.out.println(this.food);
+    }
+    public void showName(){
+        System.out.println(this.name);
+        }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick){
+        this.name = nick;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat x = new Cat();
+        System.out.println("This is");
+        x.giveNick("Barsik");
+        x.showName();
+        System.out.println("It's food is");
+        x.eat("svinina");
+        x.showFood();
+
+        Cat gav = new Cat();
+        System.out.println("There are gav's food.");
+        gav.eat("kotleta");
+        gav.showFood();
+
+        Cat black = new Cat();
+        System.out.println("There are black's food.");
+        black.eat("fish");
+        black.showFood();
     }
+
+
+
+
+
+//    public String sound() {
+//        String voice = "may-may";
+//        return voice;
+//    }
+//
+//    public static void main(String[] args) {
+//        Cat peppy = new Cat();
+//        String say = peppy.sound();
+//        System.out.println("Peppy says " + say);
+//    }
 
 }
