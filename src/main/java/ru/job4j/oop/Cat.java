763742f65789125a6f208aa2c7451dde025924b4
Biryukov-ala <abbs2305@gmail.com>
2021-11-03 14:50:29 +0700

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -38,20 +38,4 @@ public class Cat {
         black.eat("fish");
         black.showFood();
     }
-
-
-
-
-
-//    public String sound() {
-//        String voice = "may-may";
-//        return voice;
-//    }
-//
-//    public static void main(String[] args) {
-//        Cat peppy = new Cat();
-//        String say = peppy.sound();
-//        System.out.println("Peppy says " + say);
-//    }
-
 }
