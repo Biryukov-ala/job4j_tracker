7737f59e48051caf9f0e06e96814c56ca58de1f1
Biryukov-ala <abbs2305@gmail.com>
2022-03-09 13:56:32 +0700

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public String passengers(String passengers) {
+        return null;
+    }
+
+    @Override
+    public String fuel(String fuel) {
+        return null;
+    }
+}
